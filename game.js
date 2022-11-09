332adc4ed27e1417320048324e33d044b82e0386
Aux inventario <auxinventario@grupoideasa.local>
2022-11-09 23:22:38 +0100

    avance

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
 const canvas = document.querySelector ('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementsSize;
 
 function startGame () {
-    let canvasSize;
+
+    game.font = (elementsSize -5 ) + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], (elementsSize) + 7, elementsSize * i -10);    
+    }
+    
+}
+
+function setCanvasSize () {
 
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
@@ -18,13 +32,7 @@ function startGame () {
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    const elementsSize = canvasSize / 10;
+    elementsSize = canvasSize / 10;
 
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = 'end';
-
-    for (let i = 1; i < 10; i++) {
-        game.fillText(emojis['X'], elementsSize * i, elementsSize);    
-    }
-    
+    startGame ();
 }
